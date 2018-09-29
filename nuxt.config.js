a34ce6a3d17d1a5e335c25675f5579b6259547cf
Benito Gallardo <benito_gallardo@volusion.com>
2018-09-29 15:09:12 -0500

    adding local fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,8 +14,7 @@ module.exports = {
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      // { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Lato:300,400,700'}
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
 
